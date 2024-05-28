162dbb5ed4571aa2b171b68d0d68b241b5a73343
Simon <simon_wzm@163.com>
2024-05-28 14:27:10 +0800

    [main]: bugfix on dockerfile path reliance

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 
 mongorestore --uri="mongodb://localhost:27017/COURSE_DB" ./back_up/COURSE_DB/
 
-cd ./course_search_api/
+cd ./course_db_api/
 
 [ -d "node_modules" ] && exec node ./server.js || npm install && exec node ./server.js
 
